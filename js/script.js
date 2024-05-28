7fe174da041116728fb37235c70c88152adde939
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-05-28 02:53:17 +0000

    V6-04.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed EL-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check service worker support"
+*/
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-01-Osamah-Hasan/sw.js", {
+    scope: "/ICS2O-Unit-6-01-Osamah-Hasan/",
+  })
+}
+
+window.onload = function () {
+  // this calculates the volume of a sphere
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get('r')
+
+  // process
+  const volume = (4 / 3) * Math.PI * radius ** 3
+  const dimensions = 'Radius (r) = ' + radius + ' units'
+
+  // output
+  document.getElementById('radius').innerHTML = dimensions
+  document.getElementById('volume').innerHTML = 'Volume (V) = ' + volume.toFixed(2) + ' units³'
+}
